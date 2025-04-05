7c85a5ecb7a4e9a2a2269196d9a8c817256ebfd9
Rohit Yadav <rohityadav85801@gmail.com>
2025-04-05 07:27:13 +0000

    episode 4 done

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+require("./xyz") 
+
+const sum = require("./sum.js")
+
 let name = "namaste node.js";
 console.log(name);
+
+const x = 5;
+const z = 10;
+sum(x,z);
---
+++ xyz.js
@@ -0,0 +1,2 @@
+
+console.log("hello episode 4");
